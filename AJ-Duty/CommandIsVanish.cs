52710041b32848bd99d912c782d8ca87e45c6a00
Ace <46033729+AceLikesGhosts@users.noreply.github.com>
2021-02-18 14:03:27 -0800

    Publishing of all the files.
    All required files, and everything fun.

---
+++ AJ-Duty/CommandIsVanish.cs
@@ -0,0 +1,62 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+
+namespace AJUN
+{
+    class CommandIsVanish : IRocketCommand
+    {
+        public static UnturnedPlayer player2;
+
+        public string Help
+        {
+            get { return "See's if the user is in vanish."; }
+        }
+
+        public string Name
+        {
+            get { return "isvanish"; }
+        }
+
+        public string Syntax
+        {
+            get { return "[Player]"; }
+        }
+
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Both; }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>() { "duty.isvanish" };
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length == 1)
+            {
+                player2 = UnturnedPlayer.FromName(command[0]);
+                if (player2.VanishMode == true)
+                {
+                    UnturnedChat.Say(player2.CharacterName + " Has vanish enabled");
+                }
+                else
+                {
+                    UnturnedChat.Say(player2.CharacterName + " Has vanish disabled");
+                }
+            }
+
+        }
+    }
+}
---
+++ AJ-Duty/HTTP.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+
+namespace AJUN
+{
+    class HTTP
+    {
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+            using (WebClient webClient = new WebClient())
+                return webClient.UploadValues(uri, pairs);
+        }
+    }
+}
